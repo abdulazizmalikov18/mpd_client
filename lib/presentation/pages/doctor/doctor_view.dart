a43a47302a4e2bed891a1abd9ce45251d0f85ea9
abduraimovdev <kenzo778118@gmail.com>
2024-07-30 09:34:21 +0500

    Yomon code

--- lib/presentation/pages/doctor/doctor_view.dart
+++ lib/presentation/pages/doctor/doctor_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mpd_client/presentation/pages/yandex_doctor/doctor_page.dart';
 
 class DoctorView extends StatefulWidget {
   const DoctorView({super.key});
@@ -11,6 +10,11 @@ class DoctorView extends StatefulWidget {
 class _DoctorViewState extends State<DoctorView> {
   @override
   Widget build(BuildContext context) {
-    return const DoctorPage();
+    return const Scaffold(
+      body: Placeholder(),
+    );
+
+    // return const DoctorPage();
+
   }
 }
---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,29 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <file_selector_windows/file_selector_windows.h>
+#include <geolocator_windows/geolocator_windows.h>
+#include <local_auth_windows/local_auth_plugin.h>
+#include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
+#include <media_kit_video/media_kit_video_plugin_c_api.h>
+#include <screen_brightness_windows/screen_brightness_windows_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FileSelectorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FileSelectorWindows"));
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
+  LocalAuthPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("LocalAuthPlugin"));
+  MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
+  MediaKitVideoPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
+  ScreenBrightnessWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ScreenBrightnessWindowsPlugin"));
+}
