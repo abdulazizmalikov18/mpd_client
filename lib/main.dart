3338bbc57287dc86c4717c3f92773c228ccbd3e7
Abdulaziz Malikov <79694711+abdulazizmalikov18@users.noreply.github.com>
2025-05-03 16:47:12 +0500

    Update Android project configuration and dependencies; remove unused workmanager dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/rendering.dart';
@@ -10,9 +9,7 @@ import 'package:mpd_client/core/data/repository/storage_repository.dart';
 import 'package:mpd_client/core/network/fcm_service.dart';
 import 'package:mpd_client/core/utils/log_service.dart';
 import 'package:mpd_client/features/app.dart';
-import 'package:mpd_client/features/home/data/models/order_stream_model.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:workmanager/workmanager.dart';
+// import 'package:workmanager/workmanager.dart';
 // import 'package:yandex_mapkit/yandex_mapkit.dart';
 
 const String $baseUrlHttp = "https://api.t-med.uz/";
@@ -32,13 +29,13 @@ void main() async {
     }
     HttpOverrides.global = MyHttpOverrides();
 
-    if (Platform.isAndroid) {
-      Workmanager().initialize(
-        callbackDispatcher,
-        isInDebugMode: true,
-      );
-      Workmanager().registerOneOffTask("task-identifier", "nimadir");
-    }
+    // if (Platform.isAndroid) {
+    //   Workmanager().initialize(
+    //     callbackDispatcher,
+    //     isInDebugMode: true,
+    //   );
+    //   Workmanager().registerOneOffTask("task-identifier", "nimadir");
+    // }
 
     await SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
@@ -56,67 +53,67 @@ void main() async {
   });
 }
 
-@pragma('vm:entry-point')
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) async {
-    await FLNService.show(
-      title: "Socket Kuzatilmoqda ...",
-      body: "Listening Socker",
-    );
-    await StorageRepository.getInstance();
+// @pragma('vm:entry-point')
+// void callbackDispatcher() {
+//   Workmanager().executeTask((task, inputData) async {
+//     await FLNService.show(
+//       title: "Socket Kuzatilmoqda ...",
+//       body: "Listening Socker",
+//     );
+//     await StorageRepository.getInstance();
 
-    final socketURl = Uri.parse(
-      "ws://82.215.78.34/OMS/ws/work/?specialist_id=942&org_slug=mpd&lang=ru",
-    );
+//     final socketURl = Uri.parse(
+//       "ws://82.215.78.34/OMS/ws/work/?specialist_id=942&org_slug=mpd&lang=ru",
+//     );
 
-    IOWebSocketChannel socketChannel = IOWebSocketChannel.connect(socketURl);
+//     IOWebSocketChannel socketChannel = IOWebSocketChannel.connect(socketURl);
 
-    final stream = socketChannel.stream.asBroadcastStream();
-    stream.listen(
-      (event) async {
-        await FLNService.show(
-          title: "Socket Kuzatilmoqda ...",
-          body: "$event",
-        );
-        final data = jsonDecode(event);
-        switch (data['type']) {
-          case 'order-create':
-            {
-              OrdersStreamModel order = OrdersStreamModel.fromJson(data);
-              await FLNService.show(
-                title: "Order Created",
-                body: order.toString(),
-              );
+//     final stream = socketChannel.stream.asBroadcastStream();
+//     stream.listen(
+//       (event) async {
+//         await FLNService.show(
+//           title: "Socket Kuzatilmoqda ...",
+//           body: "$event",
+//         );
+//         final data = jsonDecode(event);
+//         switch (data['type']) {
+//           case 'order-create':
+//             {
+//               OrdersStreamModel order = OrdersStreamModel.fromJson(data);
+//               await FLNService.show(
+//                 title: "Order Created",
+//                 body: order.toString(),
+//               );
 
-              break;
-            }
-          case 'order-update':
-            {
-              OrdersStreamModel order = OrdersStreamModel.fromJson(data);
-              await FLNService.show(
-                title: "Order Updated",
-                body: order.toString(),
-              );
+//               break;
+//             }
+//           case 'order-update':
+//             {
+//               OrdersStreamModel order = OrdersStreamModel.fromJson(data);
+//               await FLNService.show(
+//                 title: "Order Updated",
+//                 body: order.toString(),
+//               );
 
-              break;
-            }
-        }
-      },
-      onError: (e) async {
-        await FLNService.show(
-          title: "Socket Kuzatilmoqda ...",
-          body: "ERROR",
-        );
-        socketChannel = IOWebSocketChannel.connect(socketURl);
-      },
-      onDone: () async {
-        await FLNService.show(
-          title: "Socket Kuzatilmoqda ...",
-          body: "Done",
-        );
-      },
-    );
+//               break;
+//             }
+//         }
+//       },
+//       onError: (e) async {
+//         await FLNService.show(
+//           title: "Socket Kuzatilmoqda ...",
+//           body: "ERROR",
+//         );
+//         socketChannel = IOWebSocketChannel.connect(socketURl);
+//       },
+//       onDone: () async {
+//         await FLNService.show(
+//           title: "Socket Kuzatilmoqda ...",
+//           body: "Done",
+//         );
+//       },
+//     );
 
-    return Future.value(true);
-  });
-}
+//     return Future.value(true);
+//   });
+// }
